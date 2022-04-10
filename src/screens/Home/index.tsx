ded13bb0f7db71eae10f65592967e03b14aa0684
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-04-10 12:02:28 +0530

    added screen styling

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#282828',
+  },
+});
 
 function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>
         A children’s story that can only be enjoyed by children is not a good children’s story in
         the slightest
