fcd2fad489d0da2db4daf5c2d7123e076166eb41
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-04-10 12:47:43 +0530

    installed drawer, added reanimated and gesture handler

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import NavigationContainer from './src/navigation';
+import 'react-native-gesture-handler';
 
 function App() {
   return (
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { NavigationContainer as Container } from '@react-navigation/native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Login, Home } from '@/screens';
 import { navigationRef } from './utils';
 
 const Stack = createNativeStackNavigator();
+// const Drawer = createDrawerNavigator();
 
 function NavigationContainer() {
   return (
     <Container ref={navigationRef}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator>
+        <Stack.Group screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Group>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </Container>
