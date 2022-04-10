fa3c4e92e38c8011e977c62e9aa9b2d5f33c30dc
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-04-10 11:24:20 +0530

    added styles

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -1,12 +1,31 @@
 import React, { ReactElement } from 'react';
-import { Button } from 'react-native';
+import { Text, Pressable, StyleSheet } from 'react-native';
 
 interface IProps {
   text: string;
 }
 
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#515151',
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderRadius: 16,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#fff',
+    letterSpacing: 3,
+  },
+});
+
 function BaseButton({ text }: IProps): ReactElement {
-  return <Button title={text} />;
+  return (
+    <Pressable style={styles.button}>
+      <Text style={styles.text}>{text}</Text>
+    </Pressable>
+  );
 }
 
 export default BaseButton;
