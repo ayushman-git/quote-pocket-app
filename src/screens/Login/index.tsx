8de188429e6dca9be568b7c265e387b4edc4c598
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-04-10 11:14:44 +0530

    added image, text ,button

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import BaseButton from '@/components/BaseButton';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#282828',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  quote: {
+    color: '#fff',
+  },
+});
 
 function Login() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Image source={require('../../assets/images/einstein.png')} />
+      <Text style={styles.title}>Quote Pocket</Text>
+      <Text style={styles.quote}>“Books were my pass to personal freedom”</Text>
+      <BaseButton text="GET STARTED" />
     </View>
   );
 }
