68bcbd12c136cfed27e30cd25e3815f4feafbeca
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-04-10 11:27:40 +0530

    proper layout login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -8,26 +8,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#282828',
+    paddingTop: '20%',
   },
   title: {
     color: '#fff',
     fontSize: 24,
     fontWeight: '600',
+    marginTop: 20,
   },
   quote: {
+    marginTop: 20,
     color: '#fff',
+  },
+  btnContainer: {
+    position: 'absolute',
+    bottom: '10%',
   },
 });
 
 function Login() {
+  const login = () => {
+    console.log('LOGIN');
+  };
   return (
     <View style={styles.container}>
       <Image source={einstein} />
       <Text style={styles.title}>Quote Pocket</Text>
       <Text style={styles.quote}>“Books were my pass to personal freedom”</Text>
-      <BaseButton text="GET STARTED" />
+      <View style={styles.btnContainer}>
+        <BaseButton text="GET STARTED" onPress={login} />
+      </View>
     </View>
   );
 }
